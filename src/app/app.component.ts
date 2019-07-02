1f7b592efeb0d12768ea3b8112c25fae50d05e66
Thiago Morais <tf_lima@terra.com.br>
2019-07-01 21:43:10 -0300

    Primeiro commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'gama-angular';
+export class AppComponent implements OnInit{
+  title = 'Thiago';
+  items = ['Ruby', 'Java', 'Javascript', 'Angular', 'Node'];
+
+  ngOnInit() {
+    setTimeout (() => {
+      this.title = 'Morais';
+    }, 2000);
+  }
+
+  novoItem() {
+    const text = prompt('Digite seu nome');
+
+    this.items.push(text);
+  }
 }
